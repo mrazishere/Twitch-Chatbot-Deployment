c2d4d62a3b5d43d45381287927f259000e3b3d2a
MrAZisHere <hello@asri.pro>
2023-02-19 06:35:35 +0800

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,9 @@ require('dotenv').config();
 module.exports = {
   apps: [
     {
-      name: "Enrolment-Manager",
+      name: "Bot-Deployment-Manager",
       watch: [`${process.env.BOT_FULL_PATH}/bot-deployment.js`],
       script: `${process.env.BOT_FULL_PATH}/bot-deployment.js`,
-      log_date_format: "YYYY-MM-DD",
-      max_memory_restart: '200M'
-    },
-    {
-      name: "Bot-Owner",
-      watch: [`${process.env.BOT_FULL_PATH}/channels/${process.env.TWITCH_USERNAME}.js`],
-      script: `${process.env.BOT_FULL_PATH}/channels/${process.env.TWITCH_USERNAME}.js`,
       log_date_format: "YYYY-MM-DD",
       max_memory_restart: '200M'
     }
